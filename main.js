789e632b7e170fc206e5bd6ce3e2e7ab3468289a
wanghao8080 <117663444@qq.com>
2019-01-15 18:10:44 +0800

    创建应用窗口（HTML 文档）

--- main.js
+++ main.js
@@ -3,9 +3,15 @@ const { app, BrowserWindow } = require('electron')
 let win
 
 const createWindow = () => {
-  win = new BrowserWindow()
+  win = new BrowserWindow({
+    width: 960,
+    height: 600,
+    minWidth: 830,
+    minHeight: 500,
+    backgroundColor: '#f8f8f8'
+  })
 
-  win.loadURL('https://ninghao.net')
+  win.loadURL(`file://${__dirname}/main.html`)
 }
 
 app.on('ready', createWindow)
