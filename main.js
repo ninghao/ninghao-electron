51f26d588c905f0734d3c75567027a10562b9211
wanghao8080 <117663444@qq.com>
2019-01-15 21:05:34 +0800

    自动重载应用（electron-reload）

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+require('electron-reload')(__dirname, {
+  electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
+})
 
 let win
 
